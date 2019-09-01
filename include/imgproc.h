b1bd3e1d8a28e72719a656202b8937a7209661d7
kentakuramochi <soratobi93@gmail.com>
2019-09-01 14:36:11 +0900

    add function: grayscale conversion

---
+++ include/imgproc.h
@@ -0,0 +1,8 @@
+#ifndef IMGPROC_H
+#define IMGPROC_H
+
+#include "img.h"
+
+void *cvt_to_gray(img_t *src, img_t *dst);
+
+#endif // IMGPROC_H
---
+++ src/imgproc.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include "imgproc.h"
+
+void *cvt_to_gray(img_t *src, img_t *dst)
+{
+    if (src->color_type != COLOR_TYPE_RGB) {
+        return NULL;
+    }
+
+    dst->color_type = COLOR_TYPE_GRAY;
+
+    for (int i = 0; i < src->width * src->height; i++) {
+        // BT.601
+        dst->data[i].gray = (0.299 * src->data[i].rgb.r +
+                             0.587 * src->data[i].rgb.g +
+                             0.114 * src->data[i].rgb.b);
+    }
+}
