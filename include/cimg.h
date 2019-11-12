9759fbaf46cdf494ecb82772315ac621bbb1d891
kentakuramochi <soratobi93@gmail.com>
2019-11-13 00:24:39 +0900

    add range checking to getelem()

--- include/cimg.h
+++ include/cimg.h
@@ -29,7 +29,7 @@ typedef struct {
     int     height;     ///< height
     int     channels;   ///< number of channels
     int     stride;     ///< stride (byte size of a row)
-    uint8_t *data;      ///< serialized pixel data
+    uint8_t *data;      ///< pixel data (row-major order, channel-interleaving)
 } cimg_t;
 
 ///
@@ -38,8 +38,8 @@ typedef struct {
 /// @param  width    [in]   image width
 /// @param  height   [in]   image height
 /// @param  channels [in]   image channels
-/// @return pointer to new image structure
-/// @note   pixel value is indefinite (NULL if failed)
+/// @return pointer to new image structure, NULL if failed
+/// @note   pixel value is indefinite
 ///
 cimg_t *cimg_create(int width, int height, int channels);
 
@@ -47,7 +47,7 @@ cimg_t *cimg_create(int width, int height, int channels);
 /// @fn     cimg_clone
 /// @brief  clone image structure
 /// @param  img [in]    pointer of clone source image structure
-/// @return pointer to clone image structure (NULL if failed)
+/// @return pointer to clone image structure, NULL if failed
 ///
 cimg_t *cimg_clone(cimg_t *img);
 
@@ -60,14 +60,20 @@ void cimg_delete(cimg_t *img);
 
 ///
 /// @fn     cimg_getelem
-/// @brief  get specified element
-/// @param  img [in]    pointer to image structure
-/// @param  x   [in]    x coordinate
-/// @param  y   [in]    y coordinate
-/// @param  ch  [in]    channel no
-/// @return pixel value of specified element (8bit unsigned)
-inline uint8_t cimg_getelem(cimg_t *img, int x, int y, int ch)
+/// @brief  get specified element of image structure
+/// @param  img [in]    pointer to target image structure
+/// @param  x   [in]    x coordinate [0, img->width)
+/// @param  y   [in]    y coordinate [0, img->height)
+/// @param  ch  [in]    channel no. [0, img->channels)
+/// @return value of specified element of target image structure, -1 if out of range
+///
+inline int cimg_getelem(cimg_t *img, int x, int y, int ch)
 {
+    if ((x < 0)  || (x >= img->width)  ||
+        (y < 0)  || (y >= img->height) ||
+        (ch < 0) || (ch >= img->channels)) {
+        return -1;
+    }
     return img->data[y * img->stride + x * img->channels + ch];
 }
 
