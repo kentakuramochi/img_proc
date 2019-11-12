b84a82f63b3baf4f8ed16ad7c1fdfc04cd8c184e
kentakuramochi <soratobi93@gmail.com>
2019-11-13 01:34:11 +0900

    add doc comment: convert

--- include/convert.h
+++ include/convert.h
@@ -1,16 +1,56 @@
+///
+/// @file   convert.h
+/// @brief  image conversion functions
+/// @author kentakuramochi
+///
+
 #ifndef CONVERT_H
 #define CONVERT_H
 
 #include "cimg.h"
 
+///
+/// @fn     rgb_to_gray
+/// @brief  get grayscale image from RGB image
+/// @param  src [in]   pointer to source RGB image
+/// @return pointer to grayscale image, NULL if failed
+///
 cimg_t *rgb_to_gray(cimg_t *src);
 
+///
+/// @fn     binarize
+/// @brief  get binary image (0/255) from grayscale image
+/// @param  src         [in]   pointer to source grayscale image
+/// @param  threshold   [in]   binarize threshold [0, 255]
+/// @return pointer to binary image, NULL if failed
+///
 cimg_t *binarize(cimg_t *src, uint8_t threshold);
 
+///
+/// @fn     binarize
+/// @brief  get binary image (0/255) from grayscale image with Otsu's method
+/// @param  src         [in]   pointer to source grayscale image
+/// @return pointer to binary image, NULL if failed
+///
 cimg_t *binarize_otsu(cimg_t *src);
 
+///
+/// @fn     quantize
+/// @brief  get quantized image (0/255)
+/// @param  src         [in]   pointer to source image
+/// @param  level       [in]   quantization level [1, 256]
+/// @return pointer to quantized image, NULL if failed
+///
 cimg_t *quantize(cimg_t *src, uint8_t level);
 
-void get_hist(cimg_t *src, int histgram[256], int bin);
+///
+/// @fn     get_hist
+/// @brief  get histogram of specified image
+/// @param  src         [in]    pointer to source image
+/// @param  histogram   [out]   pointer to histogram 
+/// @param  bin         [in]    num of bins of histogram
+/// @note   length of histogram must be 256
+///
+void get_hist(cimg_t *src, int histogram[256], int bin);
 
 #endif // CONVERT_H
--- src/convert.c
+++ src/convert.c
@@ -1,8 +1,20 @@
+///
+/// @file   convert.c
+/// @brief  image conversion function
+/// @author kentakuramochi
+///
+
 #include "convert.h"
 
 #include <stddef.h>
 
-// conversion formula by ITU-R BT.601
+/// @fn     rgb_to_y_BT601
+/// @brief  convert RGB to Y of YUV by ITU-R BT.601
+/// @param  r   [in]   source R value of RGB
+/// @param  g   [in]   source G value of RGB
+/// @param  b   [in]   source B value of RGB
+/// @return luma component Y
+///
 static inline uint8_t rgb_to_y_BT601(uint8_t r, uint8_t g, uint8_t b)
 {
     return (uint8_t)(0.299 * r + 0.587 * g + 0.114 * b);
@@ -51,24 +63,26 @@ cimg_t *binarize_otsu(cimg_t *src)
 {
     int n_pix = src->width * src->height;
 
+    // count each pixel value, 0 to 255
     int histgram[256] = { 0 };
     for (int i = 0; i < (src->height * src->width); i++) {
         histgram[src->data[i]]++;
     }
 
+    // determine threshold by discriminant analysis
     uint8_t threshold = 0;
-    double  sb_max    = 0.0;
+    double  sb_max    = 0.0; // max inner-class variance
 
     for (int th = 0; th < UINT8_MAX; th++) {
-        int    n0 = 0, n1 = 0;
-        double w0, w1;
-        double m0 = 0.0 , m1 = 0.0;
+        int    n0 = 0, n1 = 0;      // pixel count of class 0/1
+        double w0, w1;              // pixel ratio of class 0/1
+        double m0 = 0.0 , m1 = 0.0; // mean of class 0/1
 
+        // categorize values to class 0/1 by temporal threshold
         for (int i = 0; i < th; i++) {
             n0 += histgram[i];
             m0 += i * histgram[i];
         }
-
         for (int i = th; i <= UINT8_MAX; i++) {
             n1 += histgram[i];
             m1 += i * histgram[i];
@@ -79,14 +93,17 @@ cimg_t *binarize_otsu(cimg_t *src)
         m0 /= n0;
         m1 /= n1;
 
+        // inner-class variance
         double sb = w0 * w1 * (m0 - m1) * (m0 - m1);
 
+        // save threshold maximize inner-class variance
         if (sb > sb_max) {
             threshold = th;
             sb_max    = sb;
         }
     }
 
+    // binarize by the detemined threshold
     return binarize(src, threshold);
 }
 
@@ -97,11 +114,13 @@ cimg_t *quantize(cimg_t *src, uint8_t level)
         return NULL;
     }
 
+    // get quantized value per level
     uint8_t q_unit = 256 / level;
 
     for (int i = 0; i < (src->channels * src->height * src->width); i++) {
         uint8_t thresh = 0;
 
+        // quantize by comparing threshold: (quantized value)/2
         for (int j = 0; j < level; j++) {
             uint8_t qval =  (thresh + 0.5 * q_unit);
 
@@ -116,32 +135,35 @@ cimg_t *quantize(cimg_t *src, uint8_t level)
     return dst;
 }
 
-void get_hist(cimg_t *src, int histgram[256], int bin)
+void get_hist(cimg_t *src, int histogram[256], int bin)
 {
     for (int i = 0; i < 256; i++) {
-        histgram[i] = 0;
+        histogram[i] = 0;
     }
 
+    // bin range
     uint8_t level = 256 / bin;
 
     for (int i = 0; i < (src->width * src->height); i++) {
         uint8_t val = src->data[i];
 
+        // count each bin and save it to start position of each bin
         int th = level;
         for (int j = 0; j <= bin; j++) {
             if (val < th) {
-                histgram[(th - level)]++;
+                histogram[(th - level)]++;
                 break;
             }
             th += level;
         }
     }
 
+    // copy the count to the same bin
     int bin_s = 0;
     for (int i = 1; i < 256; i++) {
         if (i % level == 0) {
             bin_s += level;
         }
-        histgram[i] = histgram[bin_s];
+        histogram[i] = histogram[bin_s];
     }
 }
